521230801136a4386086fbff784a6278563b40fe
Souless22 <sadarahmed122@gmail.com>
2025-07-24 07:40:57 +0600

    Update userroutercontroller.js

--- backend/routerControllers/userroutercontroller.js
+++ backend/routerControllers/userroutercontroller.js
@@ -1,6 +1,15 @@
-export const userRegister=async()=>{
+import bcrypt from 'bcryptjs';
+import User from '../schema/users.js';
+
+
+export const userRegister=async(req,res)=>{
     try {
         const{fullname, username, email, gender, password, displayPicture} = req.body;
+        const user = await User.findOne({username,email});
+        if (user) return res.status(500).send({success: false, message: "User already exists"});
+        const hashpassword = bcrypt.hashSync(password, 8);
+        const boy = displayPicture || 'https://www.vecteezy.com/vector-art/439863-vector-users-icon?username=$(username)';
+        const girl = displayPicture || 'https://www.vecteezy.com/vector-art/1993889-beautiful-latin-woman-avatar-character-icon?username=$(username);'
 
     } catch (error) {
         
