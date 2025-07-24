d230fd850506804ffa68dc7647a21e65752a0dde
Souless22 <sadarahmed122@gmail.com>
2025-07-24 07:29:42 +0600

    fixing on user auth

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import dbconnect from './DB/dbconnect';
+import dbconnect from './DB/dbconnect.js';
+import authRouter from './route/authuser.js';
 
 const app = express();
 
 dotenv.config();
 
+app.use('/api/auth',authRouter)
+
 app.get('/', (req,res) => {
     res.send('Server is running');
 })
+
+
 
 const PORT = process.env.PORT || 3000;
 
---
+++ backend/routerControllers/userroutercontroller.js
@@ -0,0 +1,8 @@
+export const userRegister=async()=>{
+    try {
+        const{fullname, username, email, gender, password, displayPicture} = req.body;
+
+    } catch (error) {
+        
+    }
+}
