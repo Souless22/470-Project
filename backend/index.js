17eed55f3da4c12978c0bdfb36ca79c9d92ee5e0
Souless <sadarahmed122@gmail.com>
2025-08-05 17:27:54 +0600

    QOL update
    Removed build and test methods

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,9 @@ app.use('/api/auth',authRouter)
 app.use('/api/message',messageRouter)
 app.use('/api/user',userRouter)
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
+
+
 
 const PORT = process.env.PORT || 3000
 
